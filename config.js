7b6aada6e6303ef75da778617a3b011c0c4c5f09
ajtechdeveloper <techdeveloper.aj@gmail.com>
2024-06-30 23:19:00 +0530

    Initial code commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 export const CONFIG = {
-  API_KEY: process.env.REACT_APP_WEATHER_API_KEY,
+  API_KEY: process.env.REACT_APP_WEATHER_API_K,
   BASE_URL: 'https://api.openweathermap.org/data/2.5/weather',
 };
